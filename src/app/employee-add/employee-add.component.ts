2487276bb689fbd7c7dd0134eb2dcc4793974d82
hilal <nurul.hilal@telogic.com.sg>
2025-03-27 18:44:04 +0700

    refactor: clean up imports and improve code formatting in employee components

--- src/app/employee-add/employee-add.component.ts
+++ src/app/employee-add/employee-add.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
-import {EmployeeService, GroupEmployeeService} from '../service'
+import { EmployeeService, GroupEmployeeService } from '../service';
 
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-employee-add',
   templateUrl: './employee-add.component.html',
-  styleUrls: ['./employee-add.component.scss']
+  styleUrls: ['./employee-add.component.scss'],
 })
 export class EmployeeAddComponent {
   employee = {
@@ -20,35 +20,34 @@ export class EmployeeAddComponent {
     basicsalary: 0,
     status: 'Active',
     group: '',
-    description: ''
+    description: '',
   };
-  today = new Date()
-  optionCurrency={ 
-    prefix: 'Rp. ', 
-    thousands: '.', 
-    align :'left',
-    precision :0,
-    allowNegative :false
+  today = new Date();
+  optionCurrency = {
+    prefix: 'Rp. ',
+    thousands: '.',
+    align: 'left',
+    precision: 0,
+    allowNegative: false,
   };
 
-  items = []
+  items = [];
   constructor(
-    private http: HttpClient, 
+    private http: HttpClient,
     private router: Router,
     private employeeService: EmployeeService,
     private groupService: GroupEmployeeService
-
   ) {
-    this.getGroup()
+    this.getGroup();
   }
 
   createEmployee(): void {
-    this.employeeService.create(this.employee).subscribe((res)=>{
+    this.employeeService.create(this.employee).subscribe((res) => {
       Swal.fire({
-        text: "Sukses Menambahkan Data",
-        icon: "success"
-      }).then(()=>{
-        this.employee= {
+        text: 'Sukses Menambahkan Data',
+        icon: 'success',
+      }).then(() => {
+        this.employee = {
           username: '',
           firstname: '',
           lastname: '',
@@ -57,31 +56,17 @@ export class EmployeeAddComponent {
           basicsalary: 0,
           status: 'Active',
           group: '',
-          description: ''
-        }
-      })
-    })
-    // this.http.get<any[]>('/assets/data.json').subscribe((data) => {
-    //   const newEmployee = { id: data.length + 1, ...this.employee };
-    //   data.push(newEmployee);
-
-    //   // Save the updated data back to the JSON file (requires a backend API)
-    //   this.http.post('http://localhost:3000/api/save-data', data).subscribe(() => {
-    //     console.log('Employee added successfully');
-    //     Swal.fire({
-    //       text: "Employee added successfully",
-    //       icon: "success"
-    //     });
-    //   });
-    // });
+          description: '',
+        };
+      });
+    });
   }
-  onBack(){
-    this.router.navigate(['/employee-list'])
-
+  onBack() {
+    this.router.navigate(['/employee-list']);
   }
-  getGroup(){
-    this.groupService.getGroup().subscribe((res:any)=>{
-      this.items=res
-    })
+  getGroup() {
+    this.groupService.getGroup().subscribe((res: any) => {
+      this.items = res;
+    });
   }
 }
--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -4,13 +4,13 @@ import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { Router } from '@angular/router';
-import Swal from 'sweetalert2'
-import {EmployeeService} from '../service'
+import Swal from 'sweetalert2';
+import { EmployeeService } from '../service';
 
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
-  styleUrls: ['./employee-list.component.scss']
+  styleUrls: ['./employee-list.component.scss'],
 })
 export class EmployeeListComponent implements OnInit {
   displayedColumns: string[] = [
@@ -24,17 +24,17 @@ export class EmployeeListComponent implements OnInit {
     'status',
     // 'group',
     // 'description',
-    'action'
+    'action',
   ];
   dataSource = new MatTableDataSource<any>();
-  filter:any={username:'', email:''}
-  isLoading:boolean=false
+  filter: any = { username: '', email: '' };
+  isLoading: boolean = false;
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
   constructor(
-    private http: HttpClient, 
-    private router:Router,
+    private http: HttpClient,
+    private router: Router,
     private employeeService: EmployeeService
   ) {}
 
@@ -42,25 +42,24 @@ export class EmployeeListComponent implements OnInit {
     const navigation = window.history.state;
 
     if (navigation && navigation.filter) {
-      this.filter=navigation.filter
-      this.applyFilter()
-
+      this.filter = navigation.filter;
+      this.applyFilter();
     }
-    this.getData()
+    this.getData();
   }
-  getData(){
-    this.isLoading=true
-    this.employeeService.getEmployee().subscribe((res:any)=>{
-      setTimeout(()=>{
-        this.isLoading=false
-        this.dataSource.data = res;
-
-      },500)
-
-    },err=>{
-      this.isLoading=false
-
-    })
+  getData() {
+    this.isLoading = true;
+    this.employeeService.getEmployee().subscribe(
+      (res: any) => {
+        setTimeout(() => {
+          this.isLoading = false;
+          this.dataSource.data = res;
+        }, 500);
+      },
+      (err) => {
+        this.isLoading = false;
+      }
+    );
   }
 
   ngAfterViewInit(): void {
@@ -69,74 +68,53 @@ export class EmployeeListComponent implements OnInit {
   }
 
   applyFilter(): void {
-    this.isLoading=true
+    this.isLoading = true;
     const filterValue = this.filter.username || this.filter.email;
-    setTimeout(()=>{
-      this.isLoading=false
+    setTimeout(() => {
+      this.isLoading = false;
       this.dataSource.filterPredicate = (data, filter) => {
-        return data.username.toLowerCase().includes(this.filter.username) && data.email.toLowerCase().includes(this.filter.email);
+        return (
+          data.username.toLowerCase().includes(this.filter.username) &&
+          data.email.toLowerCase().includes(this.filter.email)
+        );
       };
-  
+
       this.dataSource.filter = filterValue;
-    },500)
-    
+    }, 500);
   }
   editEmployee(employee: any): void {
-    this.router.navigate(['/employee-edit'], { state: { data: employee, backPage:"employee-list" } });
+    this.router.navigate(['/employee-edit'], {
+      state: { data: employee, backPage: 'employee-list' },
+    });
 
     // Add logic to navigate to an edit page or open a dialog for editing
   }
   detailEmployee(employee: any): void {
-    this.router.navigate(['/employee-detail'], { state: { data: employee, backPage:"employee-list",filter:this.filter } });
+    this.router.navigate(['/employee-detail'], {
+      state: { data: employee, backPage: 'employee-list', filter: this.filter },
+    });
 
     // Add logic to navigate to an edit page or open a dialog for editing
   }
 
   deleteEmployee(employee: any): void {
     Swal.fire({
-      title: "Are you sure?",
+      title: 'Are you sure?',
       text: "You won't be able to revert this!",
-      icon: "warning",
+      icon: 'warning',
       showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Yes, delete it!"
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
     }).then((result) => {
       if (result.isConfirmed) {
-        // this.http.delete(`http://localhost:3000/api/delete-data/${employee.id}`).subscribe({
-        //   next: () => {
-        //     const index = this.dataSource.data.findIndex((e) => e.id === employee.id);
-        //     if (index !== -1) {
-        //       this.dataSource.data.splice(index, 1);
-        //       this.dataSource._updateChangeSubscription(); // Notify the table about the data change
-        //     }
-        //     // console.log('Deleted employee from server:', employee);
-        //     Swal.fire({
-        //       title: "Deleted!",
-        //       text: "Your file has been deleted.",
-        //       icon: "success"
-        //     });
-        //   },
-        //   error: (err) => {
-        //     console.error('Error deleting employee:', err);
-        //     Swal.fire({
-        //       title: "Error!",
-        //       text: "Failed to delete the employee.",
-        //       icon: "error"
-        //     });
-        //   }
-        // });
-        this.employeeService.delete(employee.id).subscribe(()=>this.getData())
+        this.employeeService
+          .delete(employee.id)
+          .subscribe(() => this.getData());
       }
     });
-    // const index = this.dataSource.data.findIndex((e) => e.id === employee.id);
-    // if (index !== -1) {
-    //   this.dataSource.data.splice(index, 1);
-    //   this.dataSource._updateChangeSubscription(); // Notify the table about the data change
-    //   console.log('Deleted employee:', employee);
-    // }
   }
-  onAddData(){
-    this.router.navigate(['/employee-add'])
+  onAddData() {
+    this.router.navigate(['/employee-add']);
   }
 }
